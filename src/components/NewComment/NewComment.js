a9bd25b61df9eaf8150dbdf74f3f795ddc067ee5
samamoazemi <moazemisama@gmail.com>
2021-12-15 03:32:53 -0800

    Send information to server

--- src/components/NewComment/NewComment.js
+++ src/components/NewComment/NewComment.js
@@ -1,20 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
 import style from "./NewComment.module.css";
 
 const NewComment = () => {
+    const[comment, setComment] = useState({
+        name : "",
+        email : "",
+        content : "",
+    })
+
+    const nameHandler = (e) => {
+        setComment({...comment, name : e.target.value});
+    }
+    
+    const emailHandler = (e) => {
+        setComment({...comment, email : e.target.value});
+    }
+
+    const contentHandler = (e) => {
+        setComment({...comment, content : e.target.value});
+    }
+
+    const postCommentHandler = () => {
+        axios
+        .post("https://jsonplaceholder.typicode.com/comments", {...comment, postId: 10 })
+        .then(res => console.log(res.data))
+        .catch()
+    }
+
     return ( 
         <div className={style.newComment}>
-            <div>
+            <h2>Add New Comment</h2>
+            <div className={style.formControl}>
                 <label>name</label>
-                <input type="text"/>
+                <input type="text" onChange={nameHandler} />
             </div>
-            <div>
+            <div  className={style.formControl}>
                 <label>email</label>
-                <input type="email"/>
+                <input type="email" onChange={emailHandler} />
             </div>
-            <div>
+            <div className={style.formControl}>
                 <label>body</label>
-                <input type="textarea"/>
+                <input type="textarea" onChange={contentHandler} />
             </div>
+            <button onClick={postCommentHandler}>Add New Comment</button>
         </div>
      );
 }
