49056cf3459ac50183faf3efabd9212a9618b7ba
samamoazemi <moazemisama@gmail.com>
2021-12-19 02:16:51 -0800

    Add axios-interceptor and instance

--- src/services/deleteCommentService.js
+++ src/services/deleteCommentService.js
@@ -1,5 +1,11 @@
+// import instance from "../axiosInstance";
+
 import http from "./httpService";
 
 export function deleteComment (commentId) {
    return http.delete(`/comments/${commentId}`);
+
+ // return instance.delete(`/comments/${commentId}`);
+
+ // instance => we use instance when we have many API.
 }
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,33 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+
+//request,response
+
+axios.interceptors.request.use(
+    (request) => {
+      console.log(request);
+      // Edit response...
+      return request;
+    },
+    (error) => {
+        console.log(error);
+        return Promise.reject();
+    }
+)
+
+axios.interceptors.response.use(
+    (response) => {
+      console.log(response);
+      // Edit response...
+      return response;
+    },
+    (error) => {
+        console.log(error);
+        return Promise.reject();
+    }
+)
 
 const http = {
     get:axios.get,
